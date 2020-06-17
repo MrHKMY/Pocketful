598815b2c6d5a8081fb6a98dc87b9cbe331867b5
Hakimi Ridzuan <mrhakimi88@gmail.com>
2020-06-17 23:50:40 +0800

    add BottomAppBar

---
+++ BudgetWiser/app/src/main/java/com/mindscape/budgetwiser/GroceriesActivity.java
@@ -0,0 +1,18 @@
+package com.mindscape.budgetwiser;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Hakimi on 17/6/2020.
+ */
+class GroceriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_layout);
+    }
+}
