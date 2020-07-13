6b0d2eb0fecb43d4d1cc71e46ed9714df1e12a2b
Hakimi Ridzuan <mrhakimi88@gmail.com>
2020-07-13 16:43:42 +0800

    implement DarkMode (test)

--- BudgetWiser/app/src/main/java/fragments/SettingFragment.java
+++ BudgetWiser/app/src/main/java/fragments/SettingFragment.java
@@ -1,12 +1,16 @@
 package fragments;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import com.mindscape.budgetwiser.R;
@@ -16,9 +20,34 @@ import com.mindscape.budgetwiser.R;
  */
 public class SettingFragment extends Fragment {
 
+    SwitchCompat switchMode;
+    RelativeLayout mainLayout;
+    TextView textView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container,false);
+        View view = inflater.inflate(R.layout.fragment_settings, container,false);
+
+        switchMode = view.findViewById(R.id.switchDarkMode);
+        mainLayout = view.findViewById(R.id.mainRelativeSetting);
+        textView = view.findViewById(R.id.settingFragment);
+
+        switchMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (switchMode.isChecked()){
+                    mainLayout.setBackgroundColor(getResources().getColor(R.color.black));
+                    switchMode.setTextColor(getResources().getColor(R.color.white));
+                    textView.setTextColor(getResources().getColor(R.color.white));
+                } else {
+                    mainLayout.setBackgroundColor(getResources().getColor(R.color.white));
+                    switchMode.setTextColor(getResources().getColor(R.color.black));
+                    textView.setTextColor(getResources().getColor(R.color.black));
+                }
+            }
+        });
+
+        return view;
     }
 }
